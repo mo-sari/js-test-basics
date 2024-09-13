adbfea1593be90753d16aae179e1d386552be964
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-09-13 20:04:15 +0330

    TDD for factorial

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect} from 'vitest'
-import { fizzBuzz, max } from '../src/intro'
+import { factorial, fizzBuzz, max } from '../src/intro'
 
 describe('max function', () => {
     it('should return the first argument if it is greater', () => {
@@ -13,5 +13,20 @@ describe('fizzbuzz function', () => {
         expect(fizzBuzz(3)).toBe('Fizz');
         expect(fizzBuzz(5)).toBe('Buzz');
         expect(fizzBuzz(2)).toBe('2');
+    })
+})
+
+describe('Factorial TDD', () => {
+    it('should return 1 if it"s passed 0', () => {
+        expect(factorial(0)).toBe(1);
+    })
+    it('should return 1 if it"s passed 1', () => {
+        expect(factorial(1)).toBe(1);
+    })
+    it('should return 2 if it"s passed 2', () => {
+        expect(factorial(2)).toBe(2);
+    })
+    it('should return undefined if it"s passed a negative number', () => {
+        expect(factorial(-2)).toBe(undefined);
     })
 })
