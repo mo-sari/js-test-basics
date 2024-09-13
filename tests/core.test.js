fe99c65bc57198164861b4fa1ab7a5966303dae7
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-09-13 21:30:51 +0330

    Positive and negative testing and inspecting the coverage

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect} from 'vitest'
-import {getCoupons} from '../src/core';
+import {getCoupons, calculateDiscount} from '../src/core';
 
 describe('Core.getCoupons', () => {
     it('should not be empty', () => {
@@ -20,5 +20,21 @@ describe('Core.getCoupons', () => {
             expect(res).toHaveProperty('discount');
             expect(res.discount).toBeGreaterThan(0);
         })
+    })
+});
+
+describe('Core.calculateDiscount', () => {
+    it('should return a valid output if correct inputs passed', () => {
+        expect(calculateDiscount(10, 'SAVE10')).toBe(9);
+        expect(calculateDiscount(10, 'SAVE20')).toBe(8);
+    });
+    it('should return an error if wrong price type passed', () => {
+        expect(calculateDiscount('10', 'SAVE10')).toMatch(/invalid/i);
+    });
+    it('should return an error if wrong discount type passed', () => {
+        expect(calculateDiscount(10, {name:'ali'})).toMatch(/invalid/i);
+    });
+    it('should work regularly if wrong discount value passed', () => {
+        expect(calculateDiscount(10, 'Not Valid')).toBe(10);
     })
 })
