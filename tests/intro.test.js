70391b9d4460ed9cef7b52bcd888558d3ba85340
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-09-13 19:51:44 +0330

    second basic test

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,8 +1,17 @@
 import { describe, test, it, expect} from 'vitest'
-import { max } from '../src/intro'
+import { fizzBuzz, max } from '../src/intro'
 
 describe('max function', () => {
     it('should return the first argument if it is greater', () => {
         expect(max(2, 1)).toBe(2);
+    });
+    
+})
+describe('fizzbuzz function', () => {
+    it('should return fizzbuzz if it"s devisible by five', ()=>{
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+        expect(fizzBuzz(3)).toBe('Fizz');
+        expect(fizzBuzz(5)).toBe('Buzz');
+        expect(fizzBuzz(2)).toBe('2');
     })
 })
