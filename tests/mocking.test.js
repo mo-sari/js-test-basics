b73e4be8cd4958c5c348f7d6ae37139ba2709e7f
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-09-15 23:33:50 +0330

    mocking training

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,5 +1,11 @@
 import { vi, it, expect, describe } from 'vitest'
-
+import { getExchangeRate } from '../src/libs/currency';
+import { getPriceInCurrency, getShippingInfo } from '../src/mocking';
+import { getShippingQuote } from '../src/libs/shipping';
+// for mocking a function, we would have to first mock the module that 
+// contains that function
+vi.mock('../src/libs/currency');
+vi.mock('../src/libs/shipping');
 describe('test suite', () => {
     it('test case', () => {
         const greet = vi.fn();
@@ -11,4 +17,31 @@ describe('test suite', () => {
         expect(greet).toHaveBeenCalledWith('ali');
         expect(greet).toHaveBeenCalledOnce();
     })
-})
+});
+
+describe('mocking a function', () => {
+
+    it('should return price in target currency', () => {
+        vi.mocked(getExchangeRate).mockReturnValue(1.5);
+
+        const price = getPriceInCurrency(10, 'AUD');
+
+        expect(price).toBe(15);
+    });
+});
+
+describe('getShippingInfo', () => {
+
+    it('getShippingInfo should return an object is destination is valud', () => {
+        vi.mocked(getShippingQuote).mockReturnValue({cost: 10, estimatedDays: 5});
+
+        const result = getShippingInfo('qom');
+        expect(result).toMatch(/shipping cost: \$10 \(5 days\)/i);
+    });
+    it('should return unavailble if destination is not valid', () => {
+        vi.mocked(getShippingQuote).mockReturnValue(null);
+
+        const result = getShippingInfo('qom');
+        expect(result).toMatch(/unavailable/i);
+    });
+});
